8244e87f78472039f0ceb76588fb265913fafbf3
Yeon_Home <yeonjae115@gmail.com>
2020-07-08 17:23:31 +0900

    RESTful API

--- myhome/src/main/java/com/yeon/myhome/repository/BoardRepository.java
+++ myhome/src/main/java/com/yeon/myhome/repository/BoardRepository.java
@@ -3,6 +3,11 @@ package com.yeon.myhome.repository;
 import com.yeon.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
+    List<Board> findByTitleAndContent(String title, String content);
 }
